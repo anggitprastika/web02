7757eabd1623b1414f22243da171145d196cc277
anggitprastika <80105516+anggitprastika@users.noreply.github.com>
2021-04-02 23:36:43 +0700

    Completed Pertemuan 6

---
+++ pertemuan6/Pert6_latihan2.php
@@ -0,0 +1,15 @@
+<html> 
+<body> 
+<?php 
+	$x=array("one","two","three"); 
+	foreach ($x as $value) 
+ 	{ 
+ 	echo $value . "<br />"; 
+ 	} 
+?>
+
+	<br>
+ 	<p>Nama : Anggit Prastika Setiany</p>
+ 	<p>NIM &ensp;: 181011402365</p> 
+</body> 
+</html>
---
+++ pertemuan6/Pert6_latihan3.php
@@ -0,0 +1,15 @@
+<?php  
+	$UsiaKaryawan["Lisa"] = "28"; 
+	$UsiaKaryawan["Jack"] = "16"; 
+	$UsiaKaryawan["Ryan"] = "35"; 
+	$UsiaKaryawan["Rachel"] = "46"; 
+	$UsiaKaryawan["Grace"] = "34"; 
+	foreach($UsiaKaryawan as $Nama => $umur) 
+	{ 
+	echo "Nama Karyawan: $Nama, Usia: $umur"." th <br>"; 
+	} 
+
+
+echo "<br><br>Nama : Anggit Prastika Setiany";
+echo "<br>NIM &ensp;: 181011402365";
+?>
