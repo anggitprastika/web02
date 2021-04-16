1f27011d4e49102d2a85011e75a7836e977e2231
anggitprastika <80105516+anggitprastika@users.noreply.github.com>
2021-04-16 22:31:34 +0700

    Completed Pertemuan 9

---
+++ pertemuan9/Pert9_latihan4.php
@@ -0,0 +1,20 @@
+<HTML> 
+<HEAD> 
+	<TITLE> Tanggal </TITLE> 
+</HEAD> 
+<BODY> 
+	<font size="10px"> 
+	<?php 
+		echo "Sekarang tanggal "; 
+		echo date('d-F-Y'); 
+		echo "<br>dan jam "; 
+		echo date('h:i:s A'); 
+	?>
+	<center>
+		<br>
+		<p>Nama : Anggit Prastika Setiany</p>
+		<p>NIM &ensp;: 181011402365</p>
+	</center>  
+	</FONT>
+</BODY> 
+</HTML>
