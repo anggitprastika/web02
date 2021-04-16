ded00aa003ce3851964ffe8c3c4b1e6e8e2195a5
anggitprastika <80105516+anggitprastika@users.noreply.github.com>
2021-04-16 22:04:39 +0700

    Completed Pertemuan 8

---
+++ pertemuan8/Pert8_latihan1.php
@@ -0,0 +1,10 @@
+<?php 
+ 	function basic($argument) 
+ 	{ 
+ 	echo $argument; 
+ 	} 
+ 	basic('hello world!'); // outputs 'hello world!'
+
+echo "<br><br>Nama : Anggit Prastika Setiany";
+echo "<br>NIM &ensp;: 181011402365"; 
+?>
